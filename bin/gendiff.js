0fbeeb365204398543ebee32550cc1f8f578048c
Anna Petrova <petrovanna91@mail.ru>
2022-09-11 17:38:20 +0300

    Add .action and import module gendiff from index.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,11 +1,15 @@
 #!/usr/bin/env node
 
 import { program } from 'commander';
+import gendiff from '../src/index.js';
 
 program
   .description('Compares two configuration files and shows a difference.')
   .option('-V, --version', 'output the version number')
   .option('-f, --format <type>', 'output format')
   .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => {
+    console.log(gendiff(filepath1, filepath2))
+  });
 
 program.parse();
