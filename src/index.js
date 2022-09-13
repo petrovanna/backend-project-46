c7c6a7b0131481702407842328409c8cfc487b9d
Anna Petrova <petrovanna91@mail.ru>
2022-09-13 19:59:46 +0300

    Import lodash and use _.union()

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 import { readFileSync } from 'fs';
+import _ from 'lodash';
+
+const genDiff = (obj1, obj2) => {
+    const keys1 = Object.keys(obj1);
+    const keys2 = Object.keys(obj2);
+    const unitedKeys = keys1.concat(keys2);
+    const uniqeKeys = _.union(unitedKeys);
+    console.log('unitedKeys', unitedKeys);
+    console.log('uniqeKeys', uniqeKeys);
+};
 
 export default (filepath1, filepath2) => {
     const data1 = readFileSync(filepath1, 'utf-8');
@@ -6,7 +16,6 @@ export default (filepath1, filepath2) => {
     
     const data1Parse = JSON.parse(data1);
     const data2Parse = JSON.parse(data2);
-
-    console.log(data1Parse);
-    console.log(data2Parse);
+    
+    genDiff(data1Parse, data2Parse);
 };
