31f9d106d6ea0423f73c30399b29f9235927aa32
Anna Petrova <petrovanna91@mail.ru>
2022-09-14 21:11:42 +0300

    Change console.log

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -9,7 +9,7 @@ program
   .option('-f, --format <type>', 'output format')
   .arguments('<filepath1> <filepath2>')
   .action((filepath1, filepath2) => {
-    console.log(gendiff(filepath1, filepath2))
+    return(gendiff(filepath1, filepath2))
   });
 
 program.parse();
