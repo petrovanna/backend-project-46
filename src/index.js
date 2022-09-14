a250e544868d5942fb9de41b62ca41cb6f4638e8
Anna Petrova <petrovanna91@mail.ru>
2022-09-15 00:11:18 +0300

    Add using of path.resolve and process.cwd()

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { readFileSync } from 'fs';
 import _ from 'lodash';
+import path from 'path';
 
 const genDiff = (obj1, obj2) => {
     const keys1 = Object.keys(obj1);
@@ -33,15 +34,15 @@ const genDiff = (obj1, obj2) => {
     }
     const stringResult = JSON.stringify(result, null, ' ');
     const stringResultWithoutQuotes = stringResult.replaceAll('"', '')
-    console.log(stringResultWithoutQuotes.replaceAll(',', ''));
-}; // gendiff files/file1.json files/file2.json
+    return(stringResultWithoutQuotes.replaceAll(',', ''));
+}; // gendiff files/file1.json files/file2.json  gendiff file1.json file2.json
 
 export default (filepath1, filepath2) => {
-    const data1 = readFileSync(filepath1, 'utf-8');
-    const data2 = readFileSync(filepath2, 'utf-8');
+    const data1 = readFileSync(path.resolve(process.cwd(), 'files', filepath1), 'utf-8');
+    const data2 = readFileSync(path.resolve(process.cwd(), 'files', filepath2), 'utf-8');
     
     const data1Parse = JSON.parse(data1);
     const data2Parse = JSON.parse(data2);
     
-    genDiff(data1Parse, data2Parse);
+    return genDiff(data1Parse, data2Parse);
 };
