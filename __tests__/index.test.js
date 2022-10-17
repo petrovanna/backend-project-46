666f68ba43132b94f0f44398b37d60313b35a294
Anna Petrova <petrovanna91@mail.ru>
2022-10-17 19:26:16 +0300

    Add test for genDiff function

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,16 +1,18 @@
-/* import { test, expect } from 'jest';
+import { test, expect } from '@jest/globals';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import { readFileSync } from 'fs';
 import genDiff from '../src/index.js';
 
-const differences =
-{
- - follow: false
-   host: hexlet.io
- - proxy: 123.234.53.22
- - timeout: 50
- + timeout: 20
- + verbose: true
-};
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const getFixturePath = (filename) => join(__dirname, '..', '__fixtures__', filename);
+const readFixture = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
 
-test('genDiff', () => {
-  expect(genDiff(file1.json, file2.json)).toEqual(differences);
-}); */
+test('must be equal "expected_file.txt"', () => {
+  const filePath1 = getFixturePath('file1.json');
+  const filePath2 = getFixturePath('file2.json');
+  const expected = readFixture('expected_file.txt');
+
+  expect(genDiff(filePath1, filePath2)).toEqual(expected);
+});
