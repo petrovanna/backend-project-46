496acf62ad7a3f89d05e51ad5ca9bd154e6cb8c3
Anna Petrova <petrovanna91@mail.ru>
2022-10-18 13:42:01 +0300

    Add yml test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -9,9 +9,17 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => join(__dirname, '..', '__fixtures__', filename);
 const readFixture = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
 
-test('must be equal "expected_file.txt"', () => {
+test('JSON must be equal "expected_file.txt"', () => {
   const filePath1 = getFixturePath('file1.json');
   const filePath2 = getFixturePath('file2.json');
+  const expected = readFixture('expected_file.txt');
+
+  expect(genDiff(filePath1, filePath2)).toEqual(expected);
+});
+
+test('YML must be equal "expected_file.txt"', () => {
+  const filePath1 = getFixturePath('file1.yml');
+  const filePath2 = getFixturePath('file2.yml');
   const expected = readFixture('expected_file.txt');
 
   expect(genDiff(filePath1, filePath2)).toEqual(expected);
