e3ecce9f40f2eb4830d7dc502cff8968101dc8c5
Anna Petrova <petrovanna91@mail.ru>
2022-10-19 18:52:07 +0300

    Refactor tests, use test.each()

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,6 +1,6 @@
 import { test, expect } from '@jest/globals';
 import { fileURLToPath } from 'url';
-import { dirname, join, extname } from 'path';
+import { dirname, join } from 'path';
 import { readFileSync } from 'fs';
 import genDiff from '../src/index.js';
 
@@ -9,26 +9,10 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => join(__dirname, '..', '__fixtures__', filename);
 const readFixture = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
 
-const expected = readFixture('expected_file.txt');
-
-test('JSON must be equal "expected_file.txt"', () => {
-  const filePath1 = getFixturePath('file1.json');
-  const filePath2 = getFixturePath('file2.json');
-
-  console.log(extname(filePath1));
-  expect(genDiff(filePath1, filePath2)).toEqual(expected);
-});
-
-test('YML must be equal "expected_file.txt"', () => {
-  const filePath1 = getFixturePath('file1.yml');
-  const filePath2 = getFixturePath('file2.yml');
-
-  expect(genDiff(filePath1, filePath2)).toEqual(expected);
-});
-
-test('YAML must be equal "expected_file.txt"', () => {
-  const filePath1 = getFixturePath('file1.yaml');
-  const filePath2 = getFixturePath('file2.yaml');
-
-  expect(genDiff(filePath1, filePath2)).toEqual(expected);
+test.each([
+  [getFixturePath('file1.json'), getFixturePath('file2.json'), readFixture('expected_file.txt')],
+  [getFixturePath('file1.yml'), getFixturePath('file2.yml'), readFixture('expected_file.txt')],
+  [getFixturePath('file1.yaml'), getFixturePath('file2.yaml'), readFixture('expected_file.txt')],
+])('must be equal "expected_file.txt"', (filePath1, filePath2, expected) => {
+  expect(genDiff(filePath1, filePath2)).toBe(expected);
 });
