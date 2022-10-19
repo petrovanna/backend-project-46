c138896cef5681b9d30455c78e48185c27eddc9c
Anna Petrova <petrovanna91@mail.ru>
2022-10-19 18:57:20 +0300

    Add parsers.js, yaml.load

---
+++ src/parsers.js
@@ -0,0 +1,23 @@
+import { readFileSync } from 'fs';
+import path from 'path';
+import yaml from 'js-yaml';
+
+const readFile = (filePath) => {
+  const fullPath = path.resolve(process.cwd(), '__fixtures__', filePath);
+  const data = readFileSync(fullPath, 'utf-8');
+  const extension = path.extname(fullPath);
+
+  let dataParse;
+  switch (extension) {
+    case '.json':
+      dataParse = JSON.parse(data);
+      return dataParse;
+    case '.yml':
+    case '.yaml':
+      dataParse = yaml.load(data);
+      return dataParse;
+    default:
+      throw new Error(`extension ${extension} is not supported`);
+  }
+};
+export default readFile;
