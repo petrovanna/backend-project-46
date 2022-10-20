a2c48238f798867132c3ccfc193cd7b12bcc7571
Anna Petrova <petrovanna91@mail.ru>
2022-10-20 18:04:07 +0300

    Refactor parsers.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,68 @@
 import _ from 'lodash';
+import { readFileSync } from 'fs';
+import path from 'path';
+import parseFile from './parsers.js';
+
+const genDiff = (obj1, obj2) => {
+  const keys1 = Object.keys(obj1);
+  const keys2 = Object.keys(obj2);
+  const unitedKeys = keys1.concat(keys2);
+  const uniqeKeys = _.union(unitedKeys);
+  const sortedKeys = _.sortBy(uniqeKeys, [uniqeKeys.key]);
+
+  const result = {};
+
+  sortedKeys.map((key) => {
+    if (!Object.hasOwn(obj2, key)) {
+      result[` - ${key}`] = obj1[key];
+    }
+    if (!Object.hasOwn(obj1, key)) {
+      result[` + ${key}`] = obj2[key];
+    }
+    if (Object.hasOwn(obj1, key) && Object.hasOwn(obj2, key)) {
+      const value1 = obj1[key];
+      const value2 = obj2[key];
+      if (value1 !== value2) {
+        result[` - ${key}`] = obj1[key];
+        result[` + ${key}`] = obj2[key];
+      }
+      if (value1 === value2) {
+        result[`   ${key}`] = obj1[key];
+      }
+    }
+    return result;
+  });
+
+  const stringResult = JSON.stringify(result, null, ' ');
+  const stringResultWithoutQuotes = stringResult.replaceAll('"', '');
+  return (stringResultWithoutQuotes.replaceAll(',', ''));
+}; // gendiff file1.json file2.json
+// gendiff file1.yml file2.yml // gendiff file1.yaml file2.yaml
+
+const getData = (filePath) => {
+  const fullPath = path.resolve(process.cwd(), '__fixtures__', filePath);
+  const data = readFileSync(fullPath, 'utf-8');
+
+  return data;
+};
+
+const getExtension = (filePath) => path.extname(filePath);
+
+export default (filepath1, filepath2) => {
+  const data1 = getData(filepath1);
+  const data2 = getData(filepath2);
+
+  const extData1 = getExtension(filepath1);
+  const extData2 = getExtension(filepath2);
+
+  const parseData1 = parseFile(extData1, data1);
+  const parseData2 = parseFile(extData2, data2);
+
+  return genDiff(parseData1, parseData2);
+};
+
+// версия кода до вытаскивания из parsers чтения файла и расширения
+/* import _ from 'lodash';
 import parseFile from './parsers.js';
 
 const genDiff = (obj1, obj2) => {
@@ -42,4 +106,4 @@ export default (filepath1, filepath2) => {
   const data2 = parseFile(filepath2);
 
   return genDiff(data1, data2);
-};
+}; */
--- src/parsers.js
+++ src/parsers.js
@@ -1,23 +1,43 @@
-import { readFileSync } from 'fs';
+import yaml from 'js-yaml';
+
+const parseFile = (getExtension, getData) => {
+  let dataParse;
+  switch (getExtension) {
+    case '.json':
+      dataParse = JSON.parse(getData);
+      return dataParse;
+    case '.yml':
+    case '.yaml':
+      dataParse = yaml.load(getData);
+      return dataParse;
+    default:
+      throw new Error(`extension ${getExtension} is not supported`);
+  }
+};
+export default parseFile;
+
+// версия кода до вытаскивания из parsers чтения файла и расширения
+/* import { readFileSync } from 'fs';
 import path from 'path';
 import yaml from 'js-yaml';
 
 const parseFile = (filePath) => {
   const fullPath = path.resolve(process.cwd(), '__fixtures__', filePath);
   const data = readFileSync(fullPath, 'utf-8');
-  const extension = path.extname(fullPath);
+  const extension = path.extname(filePath).split('.');
+  const fileExtension = extension[1];
 
   let dataParse;
-  switch (extension) {
-    case '.json':
+  switch (fileExtension) {
+    case 'json':
       dataParse = JSON.parse(data);
       return dataParse;
-    case '.yml':
-    case '.yaml':
+    case 'yml':
+    case 'yaml':
       dataParse = yaml.load(data);
       return dataParse;
     default:
-      throw new Error(`extension ${extension} is not supported`);
+      throw new Error(`extension ${fileExtension} is not supported`);
   }
 };
-export default parseFile;
+export default parseFile; */
