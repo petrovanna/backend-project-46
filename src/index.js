b2076f29b184680018228ed043d3ad7821f3be9c
Anna Petrova <petrovanna91@mail.ru>
2022-10-27 23:56:57 +0300

    Change genDiff()

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import parseFile from './parsers.js';
 
 const stringify = (value, replacer = ' ', spacesCount = 1) => {
   const iter = (currentValue, depth) => {
-    if (typeof (currentValue) !== 'object' || currentValue === null) {
-      return String(currentValue);
+    if (!_.isObject(currentValue)) {
+      return `${currentValue}`;
     }
     const indentSize = depth * spacesCount;
     const currentIndent = replacer.repeat(indentSize);
@@ -22,44 +22,44 @@ const stringify = (value, replacer = ' ', spacesCount = 1) => {
   return iter(value, 1);
 };
 
+// {
+// name: /* ... */,
+// type: 'directory',
+// meta: {}, // Свойства директории
+// children: [/* ... */], // Здесь хранятся дети
+// }
+
 const genDiff = (obj1, obj2) => {
   const keys1 = Object.keys(obj1);
   const keys2 = Object.keys(obj2);
   const uniqeKeys = _.union(keys1, keys2);
 
   const sortedKeys = _.sortBy(uniqeKeys, [uniqeKeys.key]);
-  const result = {};
 
-  sortedKeys.map((key) => {
+  const tree = sortedKeys.map((key) => {
     if (!Object.hasOwn(obj2, key)) {
-      result[`- ${key}`] = obj1[key];
+      return { key, value: obj1[key], status: 'deleted' };
     }
     if (!Object.hasOwn(obj1, key)) {
-      result[`+ ${key}`] = obj2[key];
+      return { key, value: obj2[key], status: 'added' };
     }
-    if (Object.hasOwn(obj1, key) && Object.hasOwn(obj2, key)) {
-      const value1 = obj1[key];
-      const value2 = obj2[key];
-
-      if (_.isObject(value1) && _.isObject(value2)) {
-        result[`  ${key}`] = genDiff(value1, value2);
-      } else {
-        if (value1 !== value2) {
-          result[`- ${key}`] = obj1[key];
-          result[`+ ${key}`] = obj2[key];
-        }
-        if (value1 === value2) {
-          result[`  ${key}`] = obj1[key];
-        }
-      }
+    if (_.isObject(obj1[key]) && _.isObject(obj2[key])) {
+      return { key, children: genDiff(obj1[key], obj2[key]), status: 'nested' };
     }
-
-    return result;
+    if (obj1[key] !== obj2[key]) {
+      return {
+        key, value1: obj1[key], value2: obj2[key], status: 'changed',
+      };
+    }
+    return { key, value: obj1[key], status: 'unchanged' };
   });
-  const stringResult = stringify(result, ' ', 2);
 
-  return stringResult;
+  return tree;
+  // const stringResult = stringify(tree, ' ', 2);
+
+  // return stringResult;
 };
+
 // gendiff file1.json file2.json // gendiff file1.yml file2.yml // gendiff file1.yaml file2.yaml
 
 const getData = (filePath) => {
