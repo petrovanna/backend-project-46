fa68bc78f7837c48379040a3c67e71a505ae07ba
Anna Petrova <petrovanna91@mail.ru>
2022-10-31 18:46:31 +0300

    Add yaml/yml tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -11,8 +11,8 @@ const readFixture = (filename) => readFileSync(getFixturePath(filename), 'utf-8'
 
 test.each([
   [getFixturePath('before_nested.json'), getFixturePath('after_nested.json'), readFixture('expected_nested.txt')],
-  // [getFixturePath('file1.yml'), getFixturePath('file2.yml'), readFixture('expected_file.txt')],
-  // [getFixturePath('file1.yaml'), getFixturePath('file2.yaml'), readFixture('expected_file.txt')],
+  [getFixturePath('before_nested.yml'), getFixturePath('after_nested.yml'), readFixture('expected_nested.txt')],
+  [getFixturePath('before_nested.yaml'), getFixturePath('after_nested.yaml'), readFixture('expected_nested.txt')],
 ])('must be equal "expected_nested.txt"', (filePath1, filePath2, expected) => {
   expect(genDiff(filePath1, filePath2)).toBe(expected);
 });
