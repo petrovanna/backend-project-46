46b4045c07ad9d7aa824948fa7ce0af07f88704c
Anna Petrova <petrovanna91@mail.ru>
2022-11-02 01:49:47 +0300

    Add stylish

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 import { program } from 'commander';
-import gendiff from '../src/index.js';
+import gendiff, { stylish } from '../src/index.js';
 
 program
   .description('Compares two configuration files and shows a difference.')
@@ -9,7 +9,7 @@ program
   .option('-f, --format <type>', 'output format')
   .arguments('<filepath1> <filepath2>')
   .action((filepath1, filepath2) => {
-    console.log(gendiff(filepath1, filepath2));
+    console.log(stylish(gendiff(filepath1, filepath2), 1));
   });
 
 program.parse();
