b309a3bb8ef07126363ba43778c1d7a748e3145d
Anna Petrova <petrovanna91@mail.ru>
2022-11-02 16:45:54 +0300

    Add stylish.js

---
+++ src/stylish.js
@@ -0,0 +1,43 @@
+const getIndent = (depth, spacesCount = 4) => ' '.repeat(depth * spacesCount - 2);
+
+const getBracketIndent = (depth, spacesCount = 4) => ' '.repeat((depth * spacesCount) - 4);
+
+const stringify = (value, depth) => {
+  if (typeof (value) !== 'object' || value === null) {
+    return String(value);
+  }
+  const arrayOfValue = Object.entries(value);
+  const lines = arrayOfValue.map(([key, val]) => `${getIndent(depth + 1)}  ${key}: ${stringify(val, depth + 1)}`);
+  const result = ['{', ...lines, `${getIndent(depth)}  }`].join('\n');
+
+  return result;
+};
+
+const stylish = (data, depth) => {
+  const lines = data.map((item) => {
+    if (item.type === 'nested') {
+      return `${getIndent(depth)}  ${item.key}: ${stringify((stylish(item.children, depth + 1)), depth)}`;
+    }
+    if (item.type === 'deleted') {
+      return `${getIndent(depth)}- ${item.key}: ${stringify(item.value, depth)}`;
+    }
+    if (item.type === 'unchanged') {
+      return `${getIndent(depth)}  ${item.key}: ${stringify(item.value, depth)}`;
+    }
+    if (item.type === 'changed') {
+      return `${getIndent(depth)}- ${item.key}: ${stringify(item.value1, depth)}\n${getIndent(depth)}+ ${item.key}: ${stringify(item.value2, depth)}`;
+    }
+    if (item.type === 'added') {
+      return `${getIndent(depth)}+ ${item.key}: ${stringify(item.value, depth)}`;
+    }
+    return lines;
+  });
+
+  const result = ['{', ...lines, `${getBracketIndent(depth)}}`].join('\n');
+
+  return result;
+};
+export default stylish;
+// gendiff before_flat.json after_flat.json
+// gendiff before_nested.json after_nested.json
+// gendiff before_nested.yml after_nested.yml
