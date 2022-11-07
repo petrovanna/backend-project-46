edbf9f483d1818c8dd477d060528570af6a413ce
Anna Petrova <petrovanna91@mail.ru>
2022-11-07 19:15:57 +0300

    Add plain tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,17 +3,29 @@ import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
 import { readFileSync } from 'fs';
 import genDiff from '../src/index.js';
-import stylish from '../src/stylish.js';
+// import stylish from '../src/stylish.js';
+import plain from '../src/plain.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const getFixturePath = (filename) => join(__dirname, '..', '__fixtures__', filename);
 const readFixture = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
 
-test.each([
-  [getFixturePath('before_nested.json'), getFixturePath('after_nested.json'), readFixture('expected_nested.txt')],
-  [getFixturePath('before_nested.yml'), getFixturePath('after_nested.yml'), readFixture('expected_nested.txt')],
-  [getFixturePath('before_nested.yaml'), getFixturePath('after_nested.yaml'), readFixture('expected_nested.txt')],
+/* test.each([
+  [getFixturePath('before_nested.json'), getFixturePath('after_nested.json'),
+  readFixture('expected_nested.txt')],
+  [getFixturePath('before_nested.yml'), getFixturePath('after_nested.yml'),
+  readFixture('expected_nested.txt')],
+  [getFixturePath('before_nested.yaml'), getFixturePath('after_nested.yaml'),
+  readFixture('expected_nested.txt')],
 ])('must be equal "expected_nested.txt"', (filePath1, filePath2, expected) => {
   expect(stylish((genDiff(filePath1, filePath2)))).toBe(expected);
+}); */
+
+test.each([
+  [getFixturePath('before_nested.json'), getFixturePath('after_nested.json'), readFixture('expected_plain.txt')],
+  [getFixturePath('before_nested.yml'), getFixturePath('after_nested.yml'), readFixture('expected_plain.txt')],
+  [getFixturePath('before_nested.yaml'), getFixturePath('after_nested.yaml'), readFixture('expected_plain.txt')],
+])('must be equal "expected_plain.txt"', (filePath1, filePath2, expected) => {
+  expect(plain(genDiff(filePath1, filePath2))).toBe(expected);
 });
