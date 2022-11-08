f1805c1bf13712c904dd9f017f970b478ad8291e
Anna Petrova <petrovanna91@mail.ru>
2022-11-08 17:47:13 +0300

    Add formater to gendiff

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import _ from 'lodash';
 import { readFileSync } from 'fs';
 import path from 'path';
 import parseFile from './parsers.js';
-// import stylish from './stylish.js';
-import plain from './plain.js';
 
 const getKeys = (obj1, obj2) => {
   const keys1 = Object.keys(obj1);
@@ -51,7 +49,7 @@ const getExtension = (filePath) => {
   return extension[1];
 };
 
-export default (filepath1, filepath2, format = plain) => {
+export default (filepath1, filepath2) => {
   const data1 = getData(filepath1);
   const data2 = getData(filepath2);
 
@@ -61,5 +59,5 @@ export default (filepath1, filepath2, format = plain) => {
   const parseData1 = parseFile(extData1, data1);
   const parseData2 = parseFile(extData2, data2);
 
-  return format(genDiff(parseData1, parseData2));
+  return (genDiff(parseData1, parseData2));
 };
