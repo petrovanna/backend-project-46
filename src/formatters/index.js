edc074aa2c954430d68a23a1666dc169df9697c5
Anna Petrova <petrovanna91@mail.ru>
2022-11-08 20:04:36 +0300

    Add json format

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -7,8 +7,13 @@ const formater = (data, format = 'stylish') => {
       return stylish(data);
     case 'plain':
       return plain(data);
+    case 'json':
+      return JSON.stringify(data);
     default:
       throw new Error(`format ${format} is not supported`);
   }
 };
 export default formater;
+// gendiff -f stylish before_nested.json after_nested.json
+// gendiff -f plain before_nested.json after_nested.json
+// gendiff -f json before_nested.json after_nested.json
