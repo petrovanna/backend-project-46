266fa889674fe0eaa630bacee4095bbf7590d027
Anna Petrova <petrovanna91@mail.ru>
2022-11-10 23:28:58 +0300

    Move genDiff and getKeys to genDiff.js

--- src/index.js
+++ src/index.js
@@ -1,44 +1,12 @@
-import _ from 'lodash';
+// import _ from 'lodash';
 import { readFileSync } from 'fs';
 import path from 'path';
 import parseFile from './parsers.js';
 import formater from './formatters/index.js';
-
-const getKeys = (obj1, obj2) => {
-  const keys1 = Object.keys(obj1);
-  const keys2 = Object.keys(obj2);
-  const uniqeKeys = _.union(keys1, keys2);
-  const sortedKeys = _.sortBy(uniqeKeys, [uniqeKeys.key]);
-
-  return sortedKeys;
-};
-
-const genDiff = (obj1, obj2) => {
-  const keys = getKeys(obj1, obj2);
-
-  const tree = keys.map((key) => {
-    if (!Object.hasOwn(obj2, key)) {
-      return { key, value: obj1[key], type: 'deleted' };
-    }
-    if (!Object.hasOwn(obj1, key)) {
-      return { key, value: obj2[key], type: 'added' };
-    }
-    if (_.isObject(obj1[key]) && _.isObject(obj2[key])) {
-      return { key, children: genDiff(obj1[key], obj2[key]), type: 'nested' };
-    }
-    if (obj1[key] !== obj2[key]) {
-      return {
-        key, value1: obj1[key], value2: obj2[key], type: 'changed',
-      };
-    }
-    return { key, value: obj1[key], type: 'unchanged' };
-  });
-
-  return tree;
-};
+import genDiff from './genDiff.js';
 
 const getData = (filePath) => {
-  const fullPath = path.resolve(process.cwd(), '__fixtures__', filePath);
+  const fullPath = path.resolve(process.cwd(), filePath);
   const data = readFileSync(fullPath, 'utf-8');
 
   return data;
